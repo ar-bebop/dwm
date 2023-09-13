4e98ea699ed915217437e084f8ef596a02aab4d7
ar-bebop <aribeir4@gmail.com>
2023-09-13 02:18:44 -0400

    remove exitmenu patch

--- config.def.h
+++ config.def.h
@@ -70,7 +70,6 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
-#include "exitdwm.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -125,8 +124,8 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_e,      exitdwm,         {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,            {1} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,            {1} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,            {0} },
     { MODKEY,                       XK_s,      scratchpad_show, {.i = 1} },
     { MODKEY,                       XK_y,      scratchpad_show, {.i = 2} },
     { MODKEY,                       XK_u,      scratchpad_show, {.i = 3} },
